82a8674b9102324d36866732e2ca010f78cfd4c0
Brandon <bcharris713@gmail.com>
2022-04-28 13:13:57 -0500

    commit tree 0cdfad343d7317b2451a3803cdefab9c7e4024e4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+let mostProfitableNeighborhood;
+let companyCeo;
+
+const companyName = 'Scuber';
+mostProfitableNeighborhood = 'Chelsea';
+companyCeo = 'Susan Smith';
